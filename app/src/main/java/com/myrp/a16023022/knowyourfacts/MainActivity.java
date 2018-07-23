8b3909f6ccfc8568fb22c05a42c4fb716d25ce5a
lutfiah <16023022@myrp.edu.sg>
2018-07-23 16:37:40 +0800

    Initial commit

---
+++ app/src/main/java/com/myrp/a16023022/knowyourfacts/MainActivity.java
@@ -0,0 +1,64 @@
+package com.myrp.a16023022.knowyourfacts;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentAdapter adapter;
+    ViewPager vPager;
+
+    Button btnBack, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Fragment1());
+        al.add(new Fragment2());
+        al.add(new Fragment3());
+
+        adapter = new MyFragmentAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/myrp/a16023022/knowyourfacts/MyFragmentAdapter.java
@@ -0,0 +1,27 @@
+package com.myrp.a16023022.knowyourfacts;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MyFragmentAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments;
+
+    public MyFragmentAdapter(FragmentManager fm, ArrayList<Fragment> al) {
+        super(fm);
+        fragments = al;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
